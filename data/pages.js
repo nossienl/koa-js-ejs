f385740d6369524980bc30be693a5e0f4da069cb
Niels Otterspoor <niels.otterspoor@gmail.com>
2022-06-01 08:33:45 +0200

    Initial Commit

---
+++ data/pages.js
@@ -0,0 +1,59 @@
+
+export const pages = [
+    {
+        id: 'index',
+        link: '/',
+        template: 'index',
+        name: 'Home',
+        data: {
+            title: "Home",
+            people: ['niels', 'rik', 'valerie']
+        }
+    },
+    {
+        id: 'nieuws',
+        link: '/nieuws',
+        template: 'news',
+        name: 'Nieuws',
+        data: {
+            title: "Nieuws",
+        }
+    },
+    {
+        id: 'nieuws/item',
+        title: 'nieuws-item',
+        link: '/nieuws/item',
+        template: 'news/item',
+        name: 'Nieuws Item',
+        data: {
+            title: "Nieuws Item",
+        }
+    },
+    {
+        id: 'profile',
+        link: '/profile',
+        name: 'Profile Link',
+        template: 'profile',
+        data: {
+            title: "Profile"
+        }
+    },
+    {
+        id: 'about',
+        link: '/about',
+        name: 'About us',
+        template: 'about',
+        data: {
+            title: "About page",
+        }
+    },
+    {
+        id: 'contact',
+        link: '/contact',
+        name: 'Contact',
+        template: 'contact',
+        data: {
+            title: "Contact page",
+        }
+    }
+];
