c7beb3c47531598f629078171e81df90be7688f0
Niels Otterspoor <niels.otterspoor@gmail.com>
2022-06-01 08:43:25 +0200

    Update data

--- data/pages.js
+++ data/pages.js
@@ -1,59 +1,58 @@
-
 export const pages = [
-    {
-        id: 'index',
-        link: '/',
-        template: 'index',
-        name: 'Home',
-        data: {
-            title: "Home",
-            people: ['niels', 'rik', 'valerie']
-        }
+  {
+    id: "index",
+    link: "/",
+    template: "index",
+    name: "Home",
+    data: {
+      title: "Home",
+      people: ["niels", "niels 2", "niels 3"],
     },
-    {
-        id: 'nieuws',
-        link: '/nieuws',
-        template: 'news',
-        name: 'Nieuws',
-        data: {
-            title: "Nieuws",
-        }
+  },
+  {
+    id: "nieuws",
+    link: "/nieuws",
+    template: "news",
+    name: "Nieuws",
+    data: {
+      title: "Nieuws",
     },
-    {
-        id: 'nieuws/item',
-        title: 'nieuws-item',
-        link: '/nieuws/item',
-        template: 'news/item',
-        name: 'Nieuws Item',
-        data: {
-            title: "Nieuws Item",
-        }
+  },
+  {
+    id: "nieuws/item",
+    title: "nieuws-item",
+    link: "/nieuws/item",
+    template: "news/item",
+    name: "Nieuws Item",
+    data: {
+      title: "Nieuws Item",
     },
-    {
-        id: 'profile',
-        link: '/profile',
-        name: 'Profile Link',
-        template: 'profile',
-        data: {
-            title: "Profile"
-        }
+  },
+  {
+    id: "profile",
+    link: "/profile",
+    name: "Profile Link",
+    template: "profile",
+    data: {
+      title: "Profile",
     },
-    {
-        id: 'about',
-        link: '/about',
-        name: 'About us',
-        template: 'about',
-        data: {
-            title: "About page",
-        }
+  },
+  {
+    id: "about",
+    link: "/about",
+    name: "About us",
+    template: "about",
+    data: {
+      title: "About page",
     },
-    {
-        id: 'contact',
-        link: '/contact',
-        name: 'Contact',
-        template: 'contact',
-        data: {
-            title: "Contact page",
-        }
-    }
+  },
+  {
+    id: "contact",
+    link: "/contact",
+    name: "Contact",
+    template: "contact",
+    data: {
+      title: "Contact page",
+    },
+  },
 ];
